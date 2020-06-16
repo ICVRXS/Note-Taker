3fdbd0ff6be239ccdc50b9856fcfba710b8a1c0a
ICVRXS <calebhay@rocketmail.com>
2020-06-16 16:10:22 -0700

    put HTML and API routes in respective JS files within a routes folder

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/api/notes", (req, res) => {
+    //TBD
+});
+
+router.post("/api/notes", (req, res) => {
+    //TBD
+});
+
+router.delete("/api/notes/:id", (req, res) => {
+    //TBD
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,33 +1,13 @@
 const express = require("express");
-const path = require("path");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 const PORT = 8080;
 const app = express();
 
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/api/notes", (req, res) => {
-    //TBD
-});
-
-app.post("/api/notes", (req, res) => {
-    //TBD
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-    //TBD
-});
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
