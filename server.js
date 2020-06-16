72ffde6270ec2bc004303ac9a8d859340c259da3
ICVRXS <calebhay@rocketmail.com>
2020-06-16 15:55:58 -0700

    Created api routes

--- server.js
+++ server.js
@@ -13,6 +13,22 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    //TBD
+});
+
+app.post("/api/notes", (req, res) => {
+    //TBD
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    //TBD
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
 });
