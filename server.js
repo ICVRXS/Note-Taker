014112f9f0379cfc495d737b5bc20ee8f47f4919
ICVRXS <calebhay@rocketmail.com>
2020-06-16 15:19:49 -0700

    Started to add pathing

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
+const path = require("path");
 const PORT = 8080;
-
 const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
-})
+});
