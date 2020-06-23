d9fb7915231905ce1508582fb2b6bc8534bba2c1
ICVRXS <calebhay@rocketmail.com>
2020-06-23 14:41:46 -0700

    added express port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
